3a3fcbb42c4c95bb458d89f4516dc15cb0b41653
@syabdulr <abdulsyed1998@gmail.com>
2023-05-10 14:49:03 -0400

    refactored for edge cases and more clarity

--- timer1.js
+++ timer1.js
@@ -1,9 +1,21 @@
 // implement alarm clock that will beep after a specified amount of time has passed
-let alarms = process.argv.slice(2); //cmd line arguments ignoring the first 2 as it is path and file name
 
-for (let i in alarms) { // for every value in the arguments
-  setTimeout(() => {
-    // process.stdout.write('\x07'); doesnt make a sound so I have decided to use console.log to indicate pings
-    console.log('Beep!');
-  }, i * 1000); //convert miliseconds to seconds
+// Get the command-line arguments starting from the third one (the first two are "node" and the script name)
+let alarms = process.argv.slice(2).map(Number);
+
+// Sort the alarms in ascending order
+alarms.sort((a, b) => a - b); // if a-b is negative, b is bigger than a so swap places, if a-b is positive, keep places
+
+// Loop through each alarm and set a timeout for it
+for (let i in alarms) {
+  let time = alarms[i];
+
+  // Check if the alarm is a valid number greater than zero
+  if (!isNaN(time) && time > 0) { // edge cases if input isANumber and time MUST be greater than 0
+
+    // Set a timeout for the alarm, and log a message when it goes off
+    setTimeout(() => {
+      console.log('Beep!');
+    }, time * 1000); // time is in miliseconds so convert to seconds
+  }
 }
